5ff0bb35111491595c2f3d488f92084b2d88497b
MaximVoronez <maxsemerikov2012@mail.ru>
2024-08-06 00:05:09 +0300

    модуль 1 Строки и индексация строк

---
+++ Модуль 1_ Строки. индексация строк.py
@@ -0,0 +1,18 @@
+name = "example"
+print(name[0]) # первая букава
+print(name[-1]) # последняя буква
+name = ('Urban') # работа с переменной "Urban"
+print(name [2:5]) # срез слова ban
+print(name[::-1]) # Urban наоборот
+name = ("example") # вывод на экран каждую второю букву
+print(name [1::2])
+#----------------------------------
+example = 'Топинамбур'
+print(example[0]) # T
+print(example[-1]) # P
+print(example[5:10]) # Амбур
+print(example[::-1]) # рубманипоТ
+print(example[1::2]) # оиабр
+
+
+
